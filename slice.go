cdb701c49d892089c326710441857562e3f2bf80
Igor Mihalik <igor.mihalik@gmail.com>
2015-12-02 12:25:12 +0100

    used rocksdb_free to dealocate malloc()ed memory (see c.h of rocksdb)

--- slice.go
+++ slice.go
@@ -1,9 +1,8 @@
 package rdb
 
 // #include <stdlib.h>
+// #include "rocksdb/c.h"
 import "C"
-
-import "unsafe"
 
 // Slice is used as a wrapper for non-copy values
 type Slice struct {
@@ -26,7 +25,7 @@ func (self *Slice) Size() int {
 
 func (self *Slice) Free() {
 	if !self.freed {
-		C.free(unsafe.Pointer(self.data))
+		C.rocksdb_free(self.data)
 		self.freed = true
 	}
 }
