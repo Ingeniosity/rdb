c88ec1d5e62391e3197c697ac0ec0fa6e14696c6
Igor Mihalik <igor.mihalik@gmail.com>
2016-08-02 11:01:49 +0200

    update

--- ext.go
+++ ext.go
@@ -3,9 +3,15 @@ package rdb
 // #include "rocksdb/c.h"
 // #include "ext.h"
 import "C"
+import "unsafe"
 
 func (db *DB) KeyMayExist(opts *ReadOptions, key []byte) bool {
 	var cErr *C.char
 	cKey := byteToChar(key)
-	return C.rocksdb_key_may_exist(db.c, opts.c, cKey, C.size_t(len(key)), &cErr) != 0
+	ret := C.rocksdb_key_may_exist(db.c, opts.c, cKey, C.size_t(len(key)), &cErr)
+	if cErr != nil {
+		defer C.rocksdb_free(unsafe.Pointer(cErr))
+		return false
+	}
+	return ret != 0
 }
