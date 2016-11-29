640dd4e09883d26a923dff05c0f2dc4ca4fa18da
Igor Mihalik <igor.mihalik@gmail.com>
2016-11-29 10:42:45 +0100

    added lz4 support

--- dynflag.go
+++ dynflag.go
@@ -2,6 +2,6 @@
 
 package rdb
 
-// #cgo LDFLAGS: -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy
+// #cgo LDFLAGS: -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4
 // #cgo CXXFLAGS: -std=c++11
 import "C"
